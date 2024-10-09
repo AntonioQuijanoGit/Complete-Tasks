15e87b578e757498d9a25e4d38e8a6947cf86c0c
AntonioQuijanoGit <antonioquijanogit@gmail.com>
2024-10-09 11:56:24 +0200

    Add project

---
+++ src/components/Options.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import Dots from "./icons/Dots";
+import Popup from "./Popup";
+
+const Options = ({ idTask, tasks, setTasks }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOptions = (e) => {
+    console.log("option");
+    setOpen((current) => !current);
+  };
+
+  return (
+    <span className="relative" onClick={handleOptions}>
+      <Dots />
+      {open && <Popup idTask={idTask} tasks={tasks} setTasks={setTasks} />}
+    </span>
+  );
+};
+
+export default Options;
---
+++ src/helpers/getNameDay.js
@@ -0,0 +1,14 @@
+export const getNameDay = (date) => {
+  const numberDay = date.getDay();
+  const weekDays = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+
+  return weekDays[numberDay];
+};
